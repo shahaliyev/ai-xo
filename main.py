3949b3e6790f45d64652aa81b0c2a389cec6165d
anazimzada2020 <71261131+anazimzada2020@users.noreply.github.com>
2021-03-27 18:15:15 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,29 @@ import terminal as state
 import requestsAPI as req
 
 userId = "1039"
-teamId1 = "1251"
-teamId2 = "1263"
+teamId1 = "1263"
+teamId2 = "1288"
 boardSize = 3
 target = 3
 whichGames = "myOpenGames"
 count = "1"
 
-gameId = str(req.createGame(userId, teamId1, teamId2, str(boardSize), str(target)))
-
-board = [[""]*target for i in range(target)]
-
 player = "O";
 opponent = "X";
 
+gameId = str(req.createGame(userId, teamId1, teamId2, str(boardSize), str(target)))
+board = [[""]*target for i in range(target)]
+
 if not state.isTerminal(board, boardSize, target):
-    bestMove, bestMove_str = mm.makeMove(board)
+    bestMove, bestMove_str = mm.makeMove(board, boardSize, target, player, opponent)
     code, moveId = req.makeMove(userId, gameId, teamId1, bestMove_str)
     if code == "OK":
         print(bestMove)
         board[bestMove[0]][bestMove[1]] = player
 
+
 if not state.isTerminal(board, boardSize, target):
-    symbol, lastMove_str = req.getMoves(gameId, count)
+    symbol, lastMove_str = req.getMoves(userId, gameId, count)
     if symbol == opponent:
         lastMove = [int(x) for x in lastMove_str.split(",")]
         board[lastMove[0]][lastMove[1]] = opponent
