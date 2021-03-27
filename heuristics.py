9239e82ec9d1602364bf8c87a3372d0c42574173
anazimzada2020 <71261131+anazimzada2020@users.noreply.github.com>
2021-03-27 18:14:53 +0400

    Update heuristics.py

--- heuristics.py
+++ heuristics.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-
-def getScore(lines, boardSize, target):
+def getScore(lines, boardSize, target, player, opponent):
 
     score = 0
     
@@ -29,61 +28,36 @@ def getScore(lines, boardSize, target):
     return score
 
 
-def get_Scores_for_Row(board,boardSize,target):
-    
-    return  getScore(board, boardSize, target)
+def get_Scores_for_Row(board, boardSize, target, player, opponent):
+    return  getScore(board, boardSize, target, player, opponent)
 
 
-def get_Scores_for_Column(board,boardSize,target):
-    
+def get_Scores_for_Column(board, boardSize, target, player, opponent):
     transposed = np.transpose(board)
-    return  getScore(transposed, boardSize, target)
+    return  getScore(transposed, boardSize, target, player, opponent)
 
 
-def get_Scores_for_Diagonal(board,boardSize,target):
+def get_Scores_for_Diagonal(board, boardSize, target, player, opponent):
     
     diag_score = 0
     a = np.array(board)
+    all_diagonals = [a[::-1,:].diagonal(i) for i in range(-a.shape[0] + 1, a.shape[1])]
+    all_diagonals.extend(a.diagonal(i) for i in range(a.shape[1] - 1, -a.shape[0], -1))
 
-    all_diagonals = [a[::-1,:].diagonal(i) for i in range(-a.shape[0]+1,a.shape[1])]
-
-    all_diagonals.extend(a.diagonal(i) for i in range(a.shape[1]-1,-a.shape[0],-1))
-
-    for my_length in range(target,boardSize+1):
-        
-        my_diagonals = list(filter(lambda x: len(x) == my_length,all_diagonals))
-        diag_score += getScore(my_diagonals,my_length,target)
+    for my_length in range(target, boardSize + 1):
+        my_diagonals = list(filter(lambda x: len(x) == my_length, all_diagonals))
+        diag_score += getScore(my_diagonals, my_length, target, player, opponent)
         
     return diag_score
 
 
-def calculate_score(board,boardSize,target):
-    
+def calculate_score(board, boardSize, target, player, opponent):
     total_score = 0
     score_diagonals = 0
     
-    score_rows = get_Scores_for_Row(board, boardSize, target)
-    
-    score_columns = get_Scores_for_Column(board, boardSize, target)
-
-    score_diagonals = get_Scores_for_Diagonal(board,boardSize,target)
+    score_rows = get_Scores_for_Row(board, boardSize, target, player, opponent)
+    score_columns = get_Scores_for_Column(board, boardSize, target, player, opponent)
+    score_diagonals = get_Scores_for_Diagonal(board, boardSize, target, player, opponent)
     
     total_score = score_columns + score_diagonals + score_rows
-    
     return total_score
-
-
-#test
-
-# boardSize = 4
-# target = 3
-
-# board = [["X", "", "X",''],
-#          ["X", "", "X",''],
-#          ["O", '', 'O',''],
-#          ["O", "", "O",'']]  
-
-player = "X"
-opponent = "O"
-
-# calculate_score(board,boardSize,target)
