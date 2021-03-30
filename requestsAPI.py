85f796de965fbcaea846af747a7d56517c48cc0a
anazimzada2020 <71261131+anazimzada2020@users.noreply.github.com>
2021-03-30 18:34:10 +0400

    Update requestsAPI.py

--- requestsAPI.py
+++ requestsAPI.py
@@ -1,4 +1,5 @@
 import json
+import math
 import requests
 
 
@@ -41,8 +42,11 @@ def getMyGames(userId, whichGames):
     }
     
     response = requests.get(url, headers=headers)
-    
     print(response.text)
+    parsed = json.loads(response.text)
+    for key in parsed['myGames'][-1].keys():
+        lastGameId = key
+    return lastGameId
 
 
 # Makes move and returns moveId
@@ -67,7 +71,10 @@ def makeMove(userId, gameId, teamId1, move):
     response = requests.post(url, headers=headers, data=payload)
     print(response.text)
     parsed = json.loads(response.text)
-    return parsed['code'], parsed['moveId']
+    if parsed['code'] == "OK":
+        return parsed['code'], parsed['moveId']
+    if parsed['code'] == "FAIL":
+        return parsed['code'], False
 
 
 # Gets moves and return lastMove
@@ -85,7 +92,10 @@ def getMoves(userId, gameId, count):
     response = requests.get(url, headers=headers)
     print(response.text)
     parsed = json.loads(response.text)
-    return parsed['moves'][0]['symbol'], parsed['moves'][0]['move']
+    if parsed['code'] == "OK":
+        return parsed['moves'][0]['symbol'], parsed['moves'][0]['move']
+    if parsed['code'] == "FAIL":
+        return False, False
 
 
 # boardMap = 1 for Board Map, 0 for Board String
@@ -106,4 +116,9 @@ def getBoard(userId, boardMap, gameId):
     response = requests.get(url, headers=headers)
     print(response.text)
     parsed = json.loads(response.text)
-    return parsed['target']
+    x = 0
+    for item in parsed['output']:
+        if item == '-' or item == 'X' or item == 'O':
+            x += 1
+    boardSize = int(math.sqrt(x))
+    return boardSize, parsed['target']
