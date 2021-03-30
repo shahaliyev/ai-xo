98506e5a6401e0766dd6fc34bec1e0b4270dbcfb
anazimzada2020 <71261131+anazimzada2020@users.noreply.github.com>
2021-03-30 18:30:35 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,70 @@
+import time
 import minimax as mm
 import terminal as state
 import requestsAPI as req
 
-userId = "1039"
-teamId1 = "1263"
-teamId2 = "1288"
-boardSize = 3
-target = 3
-whichGames = "myOpenGames"
-count = "1"
 
-player = "O";
-opponent = "X";
+def WeStartGame(userId, teamId1, teamId2, boardSize, target):
+    
+    player = "O"
+    opponent = "X"
+    
+    gameId = str(req.createGame(userId, teamId1, teamId2, str(boardSize), str(target)))
+    board = [[""]*boardSize for i in range(boardSize)]
+    
+    while not state.isTerminal(board, boardSize, target):
+        
+        bestMove, bestMove_str = mm.makeMove(board, boardSize, target, player, opponent)
+        code, moveId = req.makeMove(userId, gameId, teamId1, bestMove_str)
+        
+        if code == "OK":
+            board[bestMove[0]][bestMove[1]] = player
+        
+        if state.isTerminal(board, boardSize, target):
+            break
+        
+        symbol, lastMove_str = req.getMoves(userId, gameId, "1")
+        
+        while symbol == player:
+            time.sleep(5)
+            symbol, lastMove_str = req.getMoves(userId, gameId, "1")
+        
+        if symbol == opponent:
+            lastMove = [int(x) for x in lastMove_str.split(",")]
+            board[lastMove[0]][lastMove[1]] = opponent
 
-gameId = str(req.createGame(userId, teamId1, teamId2, str(boardSize), str(target)))
-board = [[""]*target for i in range(target)]
 
-if not state.isTerminal(board, boardSize, target):
-    bestMove, bestMove_str = mm.makeMove(board, boardSize, target, player, opponent)
-    code, moveId = req.makeMove(userId, gameId, teamId1, bestMove_str)
-    if code == "OK":
-        print(bestMove)
-        board[bestMove[0]][bestMove[1]] = player
+def OpStartGame(userId, teamId1, teamId2):
+    
+    player = "X"
+    opponent = "O"
+    
+    gameId = req.getMyGames(userId, "myOpenGames")
+    boardSize, target = req.getBoard(userId, 0, gameId)
+    board = [[""]*boardSize for i in range(boardSize)]
+    
+    while not state.isTerminal(board, boardSize, target):
+        
+        symbol, lastMove_str = req.getMoves(userId, gameId, "1")
+        
+        while symbol == player or symbol == False:
+            time.sleep(5)
+            symbol, lastMove_str = req.getMoves(userId, gameId, "1")
+        
+        if symbol == opponent:
+            lastMove = [int(x) for x in lastMove_str.split(",")]
+            board[lastMove[0]][lastMove[1]] = opponent
+        
+        if state.isTerminal(board, boardSize, target):
+            break
+        
+        bestMove, bestMove_str = mm.makeMove(board, boardSize, target, player, opponent)
+        code, moveId = req.makeMove(userId, gameId, teamId2, bestMove_str)
+        
+        if code == "OK":
+            board[bestMove[0]][bestMove[1]] = player
 
 
-if not state.isTerminal(board, boardSize, target):
-    symbol, lastMove_str = req.getMoves(userId, gameId, count)
-    if symbol == opponent:
-        lastMove = [int(x) for x in lastMove_str.split(",")]
-        board[lastMove[0]][lastMove[1]] = opponent
+WeStartGame("1039", "1263", "1288", 3, 3)
+
+OpStartGame("1039", "1288", "1263")
